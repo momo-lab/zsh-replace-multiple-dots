bce6a777c905a9c4513dc6d37de4b2431b503008
momotaro <momotaro.n@gmail.com>
2017-04-25 11:03:36 +0900

    create new

---
+++ replace-multiple-dots.plugin.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+function replace_multiple_dots() {
+  local dots=$LBUFFER[-2,-1]
+  if [[ $dots == ".." ]]; then
+    LBUFFER=$LBUFFER[1,-3]'../.'
+  fi
+  zle self-insert
+}
+
+zle -N replace_multiple_dots
+bindkey "." replace_multiple_dots

dd2a68b031fc86e2f10f34451e0d79cdb4981bfd
momotaro <momotaro.n@gmail.com>
2021-07-20 12:21:51 +0900

    Improve conversion rule

--- replace-multiple-dots.plugin.zsh
+++ replace-multiple-dots.plugin.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 function replace_multiple_dots() {
-  local dots=$LBUFFER[-2,-1]
-  if [[ $dots == ".." ]]; then
+  local dots=$LBUFFER[-3,-1]
+  if [[ $dots =~ "^[ //\"']?\.\.$" ]]; then
     LBUFFER=$LBUFFER[1,-3]'../.'
   fi
   zle self-insert
